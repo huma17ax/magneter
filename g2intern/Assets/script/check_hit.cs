161bbe4b849de99d3a6aaeded91b6368af09fca0
osada_takaaki <gussannwa2000@gmail.com>
2021-08-16 17:39:02 +0900

    osadatakaaki_プロトタイプ_挙動

---
+++ g2intern/Assets/script/check_hit.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class check_hit : MonoBehaviour
+{
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+
+    void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.name == ("box"))
+        {
+            Debug.Log("hit");
+        }
+    }
+}
--- g2intern/Assets/script/cont_player.cs
+++ g2intern/Assets/script/cont_player.cs
@@ -7,6 +7,8 @@ public class cont_player : MonoBehaviour
     public Rigidbody2D rbody;
     public float SPEED = 1.0f;
     public float MAX_SPEED = 10.0f;
+    public bool b_conect = false;
+    
 
     void Start()
     {
@@ -15,6 +17,16 @@ public class cont_player : MonoBehaviour
 
     
     void Update()
+    {
+        player_move();
+
+        if (Input.GetKey("g"))
+        {
+            do_conect();
+        }
+    }
+
+    void player_move()
     {
         float vecX = 0.0f;
         if (Input.GetKey("a") && Input.GetKey("d"))
@@ -29,9 +41,24 @@ public class cont_player : MonoBehaviour
         {
             vecX = 1.0f;
         }
-        rbody.AddForce(new Vector2(vecX, 0.0f) * SPEED * Time.deltaTime);
+        float vecY = 0.0f;
+        if (Input.GetKey("s"))
+        {
+            vecY = -1.0f;
+        }
+        else if (Input.GetKey("w"))
+        {
+            vecY = 1.0f;
+        }
+        rbody.AddForce(new Vector2(vecX, vecY) * SPEED * Time.deltaTime);
         Vector2 vec_player = rbody.velocity;
         vec_player.x = Mathf.Clamp(vec_player.x, -MAX_SPEED, MAX_SPEED);
         rbody.velocity = vec_player;
     }
+
+    //public void do_conect(float )
+    //{
+    //    this.GetComponent<HingeJoint2D>().enabled = true;
+    //}
+
 }
