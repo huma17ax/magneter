7706096488e9fb6a09f6c38b3d4750cd91b2f3cf
kai_hiroto <hirohiro20001217@gmail.com>
2021-08-17 15:58:51 +0900

    UI追加

---
+++ g2intern/Assets/script/CreateFirework.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CreateFirework : MonoBehaviour
+{
+    public GameObject firework;
+    public GameObject leftUpObj;
+    public GameObject rightDownObj;
+    private Vector2 leftUp;
+    private Vector2 rightDown;
+    private List<GameObject> fireworks;
+    public int maxNum = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        leftUp = new Vector2(leftUpObj.transform.position.x, leftUpObj.transform.position.y);
+        rightDown = new Vector2(rightDownObj.transform.position.x, rightDownObj.transform.position.y);
+        InvokeRepeating("generateFirework", 0, 0.5f);
+        InvokeRepeating("destroyObj", 3, 0.5f);
+        fireworks = new List<GameObject>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    private void generateFirework()
+    {
+        GameObject o = Instantiate(firework);
+        float x = Random.Range(leftUp.x, rightDown.x);
+        float y = Random.Range(rightDown.y,leftUp.y);
+        //Debug.Log("x:"+x+",y:"+ y);
+        o.transform.position = new Vector2(x,y);
+        fireworks.Add(o);
+        Debug.Log(fireworks.Count);
+    }
+
+    private void destroyObj()
+    {
+        GameObject o = fireworks[0];
+        fireworks.RemoveAt(0);
+        Destroy(o);
+    }
+}
---
+++ g2intern/Assets/script/magnetPower.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class magnetPower : MonoBehaviour
+{
+    private bool isS = true;
+    //反発する力の方向
+    public Vector2 dir;
+    public float power = 100;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnTriggerStay2D(Collider2D collision)
+    {
+        if(collision.gameObject.tag == "Player")
+        {
+            GameObject player = collision.gameObject;
+            Rigidbody2D rb2 = player.GetComponent<Rigidbody2D>();
+            if (isS)
+            {
+                rb2.AddForce(dir * power);
+            }
+            else if(!isS)
+            {
+                rb2.AddForce(-dir * power);
+            }
+        }
+
+    }
+}
