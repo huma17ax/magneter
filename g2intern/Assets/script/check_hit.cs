c52ac6101b67cbbf3d1c0937e654bc42075c41e7
osada_takaaki <gussannwa2000@gmail.com>
2021-08-17 10:57:13 +0900

    反発の挙動など

--- g2intern/Assets/script/check_hit.cs
+++ g2intern/Assets/script/check_hit.cs
@@ -4,9 +4,14 @@ using UnityEngine;
 
 public class check_hit : MonoBehaviour
 {
+    public GameObject obj_player;
+    public cont_player script_cont_player;
+    float offset_anchorY;
+    public bool b_UPorUNDER;
+
     void Start()
     {
-        
+        offset_anchorY = this.transform.localPosition.y;
     }
 
     void Update()
@@ -14,11 +19,39 @@ public class check_hit : MonoBehaviour
         
     }
 
-    void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerStay2D(Collider2D other)
     {
-        if (other.gameObject.name == ("box"))
+        if (other.gameObject.CompareTag("S_pole"))
         {
-            Debug.Log("hit");
+            //bool b_onBox = (this.transform.position.y - other.transform.position.y > 0) && (this.transform.position.x > other.transform.position.x - other.transform.localScale.x / 2) && (this.transform.position.x < other.transform.position.x + other.transform.localScale.x / 2);
+            bool b_onBox = (check_position(this.transform.position, other.transform.position, other.transform.localScale) == 0);
+            script_cont_player.do_conect(offset_anchorY, b_UPorUNDER, b_onBox);
+        }
+    }
+
+    int check_position(Vector3 pos_this, Vector3 pos_hit, Vector3 scale_hit)
+    {
+        if(Mathf.Abs(pos_this.x - pos_hit.x) < scale_hit.x / 2.0f)
+        {
+            if(pos_this.y - pos_hit.y > 0)
+            {
+                return 0;
+            }
+            else
+            {
+                return 1;
+            }
+        }
+        else
+        {
+            if(pos_this.x - pos_hit.x > 0)
+            {
+                return 2;
+            }
+            else
+            {
+                return 3;
+            }
         }
     }
 }
--- g2intern/Assets/script/cont_player.cs
+++ g2intern/Assets/script/cont_player.cs
@@ -7,8 +7,13 @@ public class cont_player : MonoBehaviour
     public Rigidbody2D rbody;
     public float SPEED = 1.0f;
     public float MAX_SPEED = 10.0f;
+    public float FORCE_ROTA = 1.0f;
+    public float POWER_REPULSION = 10.0f;
     public bool b_conect = false;
-    
+    public HingeJoint2D hing2d;
+    public GameObject[] obj_checkCircle = new GameObject[2];
+    bool b_stand = true;
+    bool b_OnBox = false;
 
     void Start()
     {
@@ -18,47 +23,84 @@ public class cont_player : MonoBehaviour
     
     void Update()
     {
-        player_move();
-
-        if (Input.GetKey("g"))
+        if (!b_conect)
         {
-            do_conect();
+            //player_move();
+        }
+        else
+        {
+            conect_move();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            do_repulsion();
         }
     }
 
     void player_move()
     {
         float vecX = 0.0f;
-        if (Input.GetKey("a") && Input.GetKey("d"))
+        if (Input.GetKey("a"))
         {
-            vecX = 0.0f;
+            vecX += -1.0f;
         }
-        else if (Input.GetKey("a"))
+        if (Input.GetKey("d"))
         {
-            vecX = -1.0f;
+            vecX += 1.0f;
         }
-        else if (Input.GetKey("d"))
+        rbody.AddForce(new Vector2(vecX, 0.0f) * SPEED * Time.deltaTime);
+        //rbody.AddTorque(- FORCE_ROTA * Time.deltaTime * vecX, ForceMode2D.Impulse);
+        Vector2 vec_player = rbody.velocity;
+        vec_player.x = Mathf.Clamp(vec_player.x, -MAX_SPEED, MAX_SPEED);
+        rbody.velocity = vec_player;
+        Debug.Log(rbody.velocity.x);
+    }
+
+    void conect_move()
+    {
+        float vecX = 0.0f;
+        if (Input.GetKey("a"))
         {
-            vecX = 1.0f;
+            vecX += -1.0f;
+        }
+        if (Input.GetKey("d"))
+        {
+            vecX += 1.0f;
         }
         float vecY = 0.0f;
-        if (Input.GetKey("s"))
-        {
-            vecY = -1.0f;
-        }
-        else if (Input.GetKey("w"))
+        if (b_OnBox)
         {
             vecY = 1.0f;
         }
         rbody.AddForce(new Vector2(vecX, vecY) * SPEED * Time.deltaTime);
-        Vector2 vec_player = rbody.velocity;
-        vec_player.x = Mathf.Clamp(vec_player.x, -MAX_SPEED, MAX_SPEED);
-        rbody.velocity = vec_player;
     }
 
-    //public void do_conect(float )
-    //{
-    //    this.GetComponent<HingeJoint2D>().enabled = true;
-    //}
+    public void do_conect(float PosAnchorY, bool b_Stand, bool b_On)
+    {
+        if (!b_conect)
+        {
+            hing2d.enabled = true;
+            hing2d.anchor = new Vector2(0.0f, PosAnchorY);
+            b_conect = true;
+            b_OnBox = b_On;
+            b_stand = !b_Stand;
+        }
+    }
+
+    public void do_repulsion()
+    {
+        if (b_conect)
+        {
+            hing2d.enabled = false;
+            Vector2 vec_repulsion = (obj_checkCircle[0].transform.position - obj_checkCircle[1].transform.position).normalized;
+            if (!b_stand)
+            {
+                vec_repulsion *= -1.0f;
+            }
+            b_conect = false;
+            rbody.AddForce(vec_repulsion * POWER_REPULSION);
+        }
+    }
 
 }
